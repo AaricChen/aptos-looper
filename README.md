4a4e150f149f1e8032d5836c6a505528b212bb4d
Aaric <aaricchen@gmail.com>
2023-11-06 23:25:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+npx @aaricchen1991/aptos-faucet@latest -k <YOUR_WALLET_PRIVATE_KEY>
+```
---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+
+import { Command } from 'commander';
+import { textSync } from 'figlet';
+import { description, name, version } from './package.json';
+
+new Command()
+  .version(version)
+  .description(description)
+  .action((args) => {
+    console.log(textSync(name));
+    console.log(version);
+    console.log(description);
+    console.log('🚀 ~ file: index.ts:12 ~ args:', args);
+  })
+  .parse(process.argv);
